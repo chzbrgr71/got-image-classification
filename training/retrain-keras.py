eed70f0c9714761d5a8be25f8ef4a3f8bb197f54
Brian Redmond <brianisrunning@gmail.com>
2019-05-23 13:26:29 -0600

    fun times with keras

--- training/retrain-keras.py
+++ training/retrain-keras.py
@@ -8,18 +8,21 @@ from tensorflow.keras import layers
 import numpy as np
 import PIL.Image as Image
 import os
+import time
 
 print("TensorFlow version", tf.__version__)
 print("Keras version", tf.keras.__version__)
 
+start = time.time()
+
 CLASSIFIER_URL ="https://tfhub.dev/google/tf2-preview/mobilenet_v2/classification/2"
 IMAGE_RES = 224
 
-print("Downloading mobilenet_v2...")
+#print("Downloading mobilenet_v2...")
 
-model = tf.keras.Sequential([
-    hub.KerasLayer(CLASSIFIER_URL, input_shape=(IMAGE_RES, IMAGE_RES, 3))
-])
+#model = tf.keras.Sequential([
+#    hub.KerasLayer(CLASSIFIER_URL, input_shape=(IMAGE_RES, IMAGE_RES, 3))
+#])
 
 base_dir = './images'
 train_dir = os.path.join(base_dir, 'train')
@@ -47,3 +50,75 @@ print('total validation dog images:', num_dogs_val)
 print("--")
 print("Total training images:", total_train)
 print("Total validation images:", total_val)
+
+# hyperparameters
+BATCH_SIZE = 100  # Number of training examples to process before updating our models variables
+IMG_SHAPE  = 150  # Our training data consists of images with width of 150 pixels and height of 150 pixels
+EPOCHS = 1
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_image_generator      = ImageDataGenerator(rescale=1./255)  # Generator for our training data
+validation_image_generator = ImageDataGenerator(rescale=1./255)  # Generator for our validation data
+
+train_data_gen = train_image_generator.flow_from_directory(batch_size=BATCH_SIZE, 
+                                                           directory=train_dir, 
+                                                           shuffle=True, 
+                                                           target_size=(IMG_SHAPE,IMG_SHAPE), #(150,150) 
+                                                           class_mode='binary')
+
+val_data_gen = validation_image_generator.flow_from_directory(batch_size=BATCH_SIZE, 
+                                                              directory=validation_dir,
+                                                              shuffle=False,
+                                                              target_size=(IMG_SHAPE,IMG_SHAPE), #(150,150)
+                                                              class_mode='binary')
+
+sample_training_images, _ = next(train_data_gen) 
+
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+
+    tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    
+    tf.keras.layers.Conv2D(128, (3,3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2,2),
+    
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')
+])
+
+model.compile(optimizer='adam', 
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+model.summary()
+
+# setup TB callback
+tensorboard_cbk = keras.callbacks.TensorBoard(log_dir='/got-image-classification/tf-output/keras')
+
+history = model.fit_generator(
+    train_data_gen,
+    steps_per_epoch=int(np.ceil(total_train / float(BATCH_SIZE))),
+    epochs=EPOCHS,
+    validation_data=val_data_gen,
+    validation_steps=int(np.ceil(total_val / float(BATCH_SIZE))),
+    callbacks=[tensorboard_cbk]
+)
+
+print("Ran in {} seconds".format(time.time()-start))
+
+t = time.time()
+
+export_path = "/got-image-classification/tf-output/keras/saved_models/{}".format(int(t))
+tf.keras.experimental.export_saved_model(model, export_path)
+
+# Save entire model to a HDF5 file
+model.save('/got-image-classification/tf-output/keras/saved_models/my_model.h5')
+
+export_path
